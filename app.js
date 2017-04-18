1ed57e3e641cc67020f9c3b1afcecef0899a56da
Nila <nilavanrajamani@gmail.com>
2017-04-18 03:09:47 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+    res.send('Hello World!!!');
+});
+
+app.get('/books', function(req, res){
+    res.send('Hello Books!!!');
+});
+
+app.listen(port, process.env.IP, function(){
+    console.log(process.env.IP);
+    console.log("Listening on port "+ process.env.PORT);
+});
