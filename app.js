ff3899c155333d465c1fe0a5cfce061cda66448a
Nila <nilavanrajamani@gmail.com>
2017-04-19 14:21:36 +0000

    wed eve

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ var app = express();
 var port = process.env.PORT;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+//app.use(express.static('src/views'));
+app.set('views','./src/views');
+
+//var handlebars = require("express-handlebars");
+//app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+//app.set('view engine','.hbs');
+app.set('view engine','ejs');
 
 app.get('/', function(req, res){
-    res.send('Hello World!!!');
+    res.render('index',{nav : [{Link: '/Books', Text: 'Books'},{Link: '/Authors', Text: 'Authors'}], title: 'Hello from ejs'});
 });
 
 app.get('/books', function(req, res){
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require("nodemon");
 
 var jsFiles = ['*.js', 'src/**/*.js'];
-
-
 
 gulp.task('style', function(){
     return gulp.src(jsFiles)
@@ -14,3 +13,41 @@ gulp.task('style', function(){
             }))
             .pipe(jscs());
 });
+
+gulp.task('inject', function(){
+   var wiredep = require("wiredep").stream;
+   var inject = require("gulp-inject");
+   
+   var options = {
+       bowerJson: require("./bower.json"),
+       directory: './public/lib',
+       ignorePath: '../../public'
+   };
+   
+   var injectSrc = gulp.src(['./public/css/*.css','./public/js/*.js'],{ read: false});
+   var injectOptions = {
+       ignorePath: '/public'
+   }
+   
+   return gulp.src('./src/views/*.jade')
+              .pipe(wiredep(options))
+              .pipe(inject(injectSrc, injectOptions))
+              .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': process.env.PORT
+        },
+        watch: jsFiles
+    };
+    
+    return nodemon(options)
+            .on('restart', function(ev){
+                console.log('Restarting...');
+            });
+});
+
