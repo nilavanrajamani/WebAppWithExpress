44ffadc1a8dc9d37ecb236bbe39fbd0136e75af3
nila <nilavanrajmani@gmail.com>
2017-04-22 09:52:27 +0530

    Sunday morning

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 // var sql = require('mssql');
@@ -21,12 +26,21 @@ var port = process.env.PORT || 3000;
 var nav = [{ Link: '/Books', Text: 'Books' }, { Link: '/Authors', Text: 'Authors' }]; 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 //var port = 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+require('./src/config/passport.js')(app);
+
 //app.use(express.static('src/views'));
 app.set('views', './src/views');
+
 
 //var handlebars = require("express-handlebars");
 //app.engine('.hbs', handlebars({extname: '.hbs'}));
@@ -36,6 +50,7 @@ app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', { nav: [{ Link: '/Books', Text: 'Books' }, { Link: '/Authors', Text: 'Authors' }], title: 'Hello from ejs' });
